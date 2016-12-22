39f1cc5e112b0cfb729c454a05961fe934037920
Fahrettin <fahrettinates@gmail.com>
2016-12-22 10:46:23 +0300

    Initial Commit

---
+++ 07-TableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  07-TableView
+//
+//  Created by Fahrettin Ates on 22/12/2016.
+//  Copyright © 2016 Fahrettin Ates. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
